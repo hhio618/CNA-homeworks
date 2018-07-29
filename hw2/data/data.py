93e951103b50ec414fd9decdbd951b31f07fdfaa
hhio618 <hhio618@gmail.com>
2018-07-30 01:47:18 +0430

    py3 fix

--- hw2/data/data.py
+++ hw2/data/data.py
@@ -2,11 +2,11 @@ import numpy as np
 
 
 def load_actor_movie():
-    return np.loadtxt("hw2/data/Datasets/out.actor-movie", skiprows=1, dtype=int)
+    return np.loadtxt("data/Datasets/out.actor-movie", skiprows=1, dtype=int)
 
 
 def load_actor_movie_weighted():
-    E = np.loadtxt("hw2/data/Datasets/out.actor-movie", skiprows=1, dtype=int)
+    E = np.loadtxt("data/Datasets/out.actor-movie", skiprows=1, dtype=int)
     one = np.ones(shape=len(E))
     return np.c_[E, one]
 
